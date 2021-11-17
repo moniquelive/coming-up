3de244b1560b62ff71d277673389a2290d0df34c
Cyber Oliveira <lccro@msn.com>
2021-11-16 23:24:39 -0300

    Ideas controller + views

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,70 @@
+class IdeasController < ApplicationController
+  before_action :set_idea, only: %i[ show edit update destroy ]
+
+  # GET /ideas or /ideas.json
+  def index
+    @ideas = Idea.all
+  end
+
+  # GET /ideas/1 or /ideas/1.json
+  def show
+  end
+
+  # GET /ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+  # GET /ideas/1/edit
+  def edit
+  end
+
+  # POST /ideas or /ideas.json
+  def create
+    @idea = Idea.new(idea_params)
+
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: "Idea was successfully created." }
+        format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ideas/1 or /ideas/1.json
+  def update
+    respond_to do |format|
+      if @idea.update(idea_params)
+        format.html { redirect_to @idea, notice: "Idea was successfully updated." }
+        format.json { render :show, status: :ok, location: @idea }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /ideas/1 or /ideas/1.json
+  def destroy
+    @idea.destroy
+    respond_to do |format|
+      format.html { redirect_to ideas_url, notice: "Idea was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def idea_params
+    params.require(:idea).permit(:title, :body)
+  end
+end
---
+++ app/views/ideas/_idea.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! idea, :id, :created_at, :updated_at
+json.url idea_url(idea, format: :json)
